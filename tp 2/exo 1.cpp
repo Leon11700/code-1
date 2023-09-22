4082804aced9d88186c0f7e4990b0862d58f3861
Leon11700 <gimenez.leon@lyceejulesfil.fr>
2023-09-22 17:04:27 +0200

    exo 1

---
+++ tp 2/exo 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+    string user ("Lucien");
+    user[0] = 'j';
+    user[2] = 'i';
+    cout <<"Vous etes:" << user << endl;
+    return 0;
+}
+
+
