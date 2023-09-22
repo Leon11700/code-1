11e396236313103dcad6b9fc30eaca0903a716be
Leon11700 <gimenez.leon@lyceejulesfil.fr>
2023-09-22 17:17:07 +0200

    exo3-2

---
+++ tp 2/exo 3-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str ("Coucou les copains !");
+    for (int i(0); i<21; i++)
+    {
+        cout << str[i] << ' ';
+    }
+    cout << endl;
+    return 0;
+}
